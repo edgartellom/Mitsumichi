77d18548696761b78cd6bc760d094bb94d302164
Angel Tirado <angelteduardov@gmail.com>
2023-08-17 23:46:41 -0300

    commit varios
    filtros x años x marca x paginado post motos (con error)

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 const getAllMoto = require("../controllers/getAllMoto")
+const createMoto = require("../controllers/createMoto")
 
 const router = Router();
 
 //Rutas para las Motos
-router.get("/motos", getAllMoto)
+router.get("/motos", getAllMoto);
+
+//ruta para publicar motos
+router.post("/motos", createMoto);
 
 module.exports = router;
