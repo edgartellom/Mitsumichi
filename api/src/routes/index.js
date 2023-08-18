0d52cc9005dab6a54232327e20f9b9569e95d499
Cristianp24 <113152286+Cristianp24@users.noreply.github.com>
2023-08-17 22:16:23 -0300

    Add routes + Fix Api

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
 const getAllMoto = require("../controllers/getAllMoto")
-
+const getALLBrands = require("../controllers/getAllBrands")
+const getAllMotoModels = require("../controllers/getAllMotoModels")
 const router = Router();
 
 //Rutas para las Motos
-router.get("/motos", getAllMoto)
+router.get("/motos", getAllMoto);
+router.get("/marcas", getALLBrands);
+router.get("/modelos", getAllMotoModels)
+
+
 
 module.exports = router;
