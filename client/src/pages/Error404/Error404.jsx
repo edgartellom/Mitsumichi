8f7dc6f8c91d50ed38315606872eedfab17caf84
Alejandro <you@example.com>
2023-08-26 15:44:42 -0500

    Se intento arreglar el deploy

--- client/src/pages/Error404/Error404.jsx
+++ client/src/pages/Error404/Error404.jsx
@@ -9,7 +9,7 @@ const Error404 = () => {
 
   return (
     <section className=" flex justify-center items-center  bg-gradient-to-bl from-[#2e2e2e] via-[#292929] h-screen  to-[#202020] ">
-      <article className="flex flex-col items-center">
+      <section className="flex flex-col items-center">
         <picture className=" flex flex-wrap justify-center items-center">
           <img src={logo} alt="logo" width={200} />
           <h1 className=" text-9xl font-bold text-orange-700">404</h1>
@@ -24,7 +24,7 @@ const Error404 = () => {
             onClick={() => navigate("/")}
           />
         </section>
-      </article>
+      </section>
     </section>
   );
 };
