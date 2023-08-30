3d3597b7d9ec9ed67fce77f67e825e27e4ab0f25
alecanonm <al3jandrocan0n@gmail.com>
2023-08-30 18:26:23 -0500

    se agrego un poco de diseño en las card y se mejoro su display

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -3,7 +3,7 @@ import { Card } from "..";
 
 const Cards = ({ data }) => {
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-20 p-20 justify-center">
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(340px,1fr))] gap-20 p-20 place-items-center">
       {data.map((product) => (
         <Card key={product.id} data={product} />
       ))}
