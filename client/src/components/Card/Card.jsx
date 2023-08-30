09bcdb5ba6f98390e05bd2bcf1878df4dbcf427e
Angel Tirado <angelteduardov@gmail.com>
2023-08-30 13:48:04 -0300

    update card
    USD precio

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -25,13 +25,12 @@ const Card = ({ data }) => {
           </div>
           <a
             href="#"
-            className=" block text-black text-center hover:text-primary transition-colors duration-150 text-lg md:text-xl mb-1"
+            className="block text-black text-center hover:text-primary transition-colors duration-150 text-lg md:text-xl mb-1"
           >
             <div className="font-bold">{name} {motoModel} </div>
             <span className=" text-lg"> Tipo {tipo}</span>
           </a>
-          <p className="mb-4 font-light text-sm md:text-sm text-center text-gray-400">
-           {precio}
+          <p className="mb-4 text-sm md:text-sm text-center font-bold">USD {precio}
           </p>
           <div className="flex justify-center gap-x-3">
             <a
