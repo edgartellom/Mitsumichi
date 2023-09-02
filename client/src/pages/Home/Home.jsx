eb60a1a233c2ff2277b72e1d1bcf02a04e7fac58
alecanonm <al3jandrocan0n@gmail.com>
2023-09-01 23:35:43 -0500

    se ha mejorado la apariencia y semantica de las cards

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,13 +4,8 @@ import {
   fetchMotos,
   setFilters,
   setCurrentPage,
-  setSorts,
-  resetFilters,
-  resetSorts,
 } from "../../redux/slices/motoListSlice";
 import { fetchBrands } from "../../redux/slices/brandListSlice";
-import axios from "axios";
-import videoHome from "../../assets/video.mp4";
 import {
   Cards,
   Filters,
@@ -42,16 +37,8 @@ const Home = () => {
   };
 
   return (
-    <div className=" ">
-      {/* <video
-        autoPlay
-        muted
-        loop
-        src={videoHome}
-        className=" max-sm:hidden"
-      ></video> */}
-
-      <section className="pt-1 pb-3 bg-[#000000cc] flex flex-col w-screen">
+    <div className=" bg-gray-300">
+      <section className="pt-1 pb-3 bg-[#000000cc] flex flex-col ">
         <Filters
           marcas={brands}
           tipos={tipos}
