a74c77092751d2b230264f695d1f8bb471279067
alecanonm <al3jandrocan0n@gmail.com>
2023-09-05 22:31:51 -0500

    Se mejoro el diseño y display responsive del cart, tambien su semantica

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -67,35 +67,37 @@ const Cart = ({ setShowCart }) => {
   return (
     <Wrapper>
       {products?.length ? (
-        <div className="bg-gray-300 p-4 rounded-lg animate-slide-down">
+        <summary className="bg-gray-300 p-10 max-md:w-full max-md:h-full rounded-lg animate-slide-down">
           {cartItems}
-          <div className="flex max-md:gap-5 justify-center font-bold gap-40 text-1.5rem my-4">
-            <span className="font-bold text-2xl">Total Amount:</span>
-            <span className="font-bold text-2xl">{totalAmount}$</span>
-          </div>
-          <div className="flex max-md:flex-col justify-center gap-4 pb-3">
-            <Button
-              onClick={() => {
-                setShowCart(false);
-              }}
-              text={"Close"}
-              className=" text-white rounded-2xl shadow-none hover:scale-105"
-            />
-
-            {products.length > 0 && (
+          <article className="flex max-md:flex-col-reverse mt-8  font-bold gap-20 text-1.5rem my-4">
+            <div className=" flex gap-5 items-center">
+              <span className="font-bold text-2xl">Total Amount:</span>
+              <span className="font-bold text-2xl">{totalAmount}$</span>
+            </div>
+            <section className="flex max-md:flex-col-reverse  gap-4 pb-3">
               <Button
-                className=" bg-white rounded-2xl transform hover:scale-105 shadow-none border-solid border-2 border-black "
-                text="Order"
+                onClick={() => {
+                  setShowCart(false);
+                }}
+                text={"Close"}
+                className=" text-white rounded-2xl shadow-none hover:scale-105"
               />
-            )}
-          </div>
-        </div>
+
+              {products.length > 0 && (
+                <Button
+                  className=" bg-white rounded-2xl transform hover:scale-105 shadow-none border-solid border-2 border-black "
+                  text="Order"
+                />
+              )}
+            </section>
+          </article>
+        </summary>
       ) : (
-        <div className="bg-white p-4 rounded-lg animate-slide-down">
-          <div className="flex justify-between items-center font-bold text-1.5rem my-4">
+        <summary className="bg-gray-300   p-10 rounded-lg animate-slide-down">
+          <section className="flex justify-between items-center font-bold text-1.5rem my-4">
             <span className="font-bold text-2xl">Carrito vacio</span>
-          </div>
-          <div className="flex justify-center gap-2">
+          </section>
+          <section className="flex justify-center gap-2">
             <Button
               onClick={() => {
                 setShowCart(false);
@@ -103,8 +105,8 @@ const Cart = ({ setShowCart }) => {
               text={"Close"}
               className=" text-white rounded-2xl shadow-none hover:scale-105"
             />
-          </div>
-        </div>
+          </section>
+        </summary>
       )}
     </Wrapper>
   );
