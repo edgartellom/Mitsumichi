2a41b23a168324bfd8e5656467dd3bfacf294ce6
alecanonm <al3jandrocan0n@gmail.com>
2023-09-12 10:10:46 -0500

    Navbar es fixed ahora

--- client/src/helper/RootLayout.jsx
+++ client/src/helper/RootLayout.jsx
@@ -3,13 +3,15 @@ import { Navbar, Footer, AddButton } from "../components";
 const RootLayot = () => {
   const { pathname } = useLocation();
   return (
-    <>
-      {pathname !== "/" && !pathname.includes("/dashboard") && <Navbar />}
+    <div>
+      <header className=" sticky top-0 bg-white z-30">
+        {pathname !== "/" && !pathname.includes("/dashboard") && <Navbar />}
+      </header>
       {pathname === "/home" && <AddButton />}
       <main className=" flex-1 bg-gray-300">{<Outlet />}</main>
 
       {pathname !== "/" && !pathname.includes("/dashboard") && <Footer />}
-    </>
+    </div>
   );
 };
 
