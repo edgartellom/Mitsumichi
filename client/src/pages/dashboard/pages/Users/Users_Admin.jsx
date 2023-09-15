9a5d862d16ac5f59e6ce90794aab58043abdc0d5
alecanonm <al3jandrocan0n@gmail.com>
2023-09-14 20:59:43 -0500

    trabajando en la tabla de usuarios

--- client/src/pages/dashboard/pages/Users/Users_Admin.jsx
+++ client/src/pages/dashboard/pages/Users/Users_Admin.jsx
@@ -1,25 +1,19 @@
-import React, { useEffect, useState } from "react";
-import getAllUsers from "../../../../firebase/getAllUsers";
-import getInvoicesByUser from "../../../../firebase/getInvoicesByUser";
+import React, { useContext } from "react";
+import { userAuth } from "../../../../context/Auth-context";
 const Users_Admin = () => {
-  const [users, setUsers] = useState([]);
+  const { users, invoices } = useContext(userAuth);
 
-  useEffect(() => {
-    (async () => {
-      const response = await getAllUsers();
-      setUsers(response);
-    })();
-  }, []);
+  const invoicesToArr = Object.values(invoices);
 
-  console.log(users, "users");
+  console.log(invoicesToArr, "invoicesToArr");
 
   return (
-    <div className=" bg-slate-100 p-4">
+    <div className=" bg-slate-100 pt-10">
       <table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
         <thead className="bg-blue-500 text-white">
           <tr>
             <th className="px-4 border-black  border-2 text-left">
-              <input type="checkbox" />
+              <input type="checkbox" className="w-6 h-6" />
             </th>
             <th className="py-2 px-4 border-2 border-black  text-left">ID</th>
             <th className="py-2 px-4 border-2 border-black  text-left">
@@ -27,7 +21,7 @@ const Users_Admin = () => {
             </th>
             <th className="py-2 px-4 border-2 border-black  text-left">ROLE</th>
             <th className="py-2 px-4 border-2 border-black  text-left">
-              NO. ORDEN
+              Cant. ORDENES
             </th>
             <th className="py-2 px-4 border-2 border-black  text-left">
               STATUS
@@ -40,18 +34,27 @@ const Users_Admin = () => {
         <tbody>
           {users.map((user, index) => {
             const id = index + 1;
-
+            const precioTOTAL = invoicesToArr[index]
+              ?.map((item) => item[0].precio)
+              .map(Number)
+              .filter((item) => !isNaN(item))
+              .reduce((a, b) => a + b, 0);
+            console.log(precioTOTAL, "precioTOTAL");
             return (
-              <tr key={id} className=" bg-white border-2 ">
-                <td className=" px-4 bg-slate-100  border-2">
-                  <input type="checkbox" />
+              <tr key={id} className=" bg-white border-2 font-bold  ">
+                <td className=" px-4 bg-slate-100  border-2 ">
+                  <input type="checkbox" className="w-6 h-6" />
                 </td>
                 <td className="py-2 px-4">{id}</td>
                 <td className="py-2 px-4">{user.data?.name}</td>
-                <td className="py-2 px-4">{user.role}</td>
-                <td className="py-2 px-4">NO. ORDEN</td>
-                <td className="py-2 px-4">STATUS</td>
-                <td className="py-2 px-4">TOTAL PAYMENT</td>
+                <td className="py-2 px-4 text-center">{user.role}</td>
+                <td className="py-2 px-4 text-center">
+                  {invoicesToArr[index]?.length}
+                </td>
+                <td className="py-2 px-4 text-center">ACTIVE</td>
+                <td className="py-2 px-4 text-center text-orange-600">
+                  {precioTOTAL} $
+                </td>
               </tr>
             );
           })}
