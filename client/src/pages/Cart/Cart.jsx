ca480473362399801198ad3e8c3f72803ed20d39
alecanonm <al3jandrocan0n@gmail.com>
2023-09-16 01:11:36 -0500

    se ha mejorado la logica de negocio

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -1,17 +1,15 @@
 import Wrapper from "../../helper/Wrapper";
 import CartItem from "./CartItem";
 import getCartProducts from "../../firebase/getCartProducts";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { userAuth } from "../../context/Auth-context";
 import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "../../firebase/credenciales";
 import Button from "../../components/UI/Button";
 import { useNavigate } from "react-router-dom";
-import clearCart from "../../firebase/clearCart";
-import createBill from "../../firebase/createBill";
 const Cart = ({ setShowCart }) => {
-  const { currentUser, setProducts, products } = useContext(userAuth);
-  // const [products, setProducts] = useState([]);
+  const { currentUser, setProducts, products, productsLocalStorage } =
+    useContext(userAuth);
   const navigate = useNavigate();
   const gettingProducts = async () =>
     !currentUser ? [] : await getCartProducts(currentUser.uid);
@@ -25,6 +23,7 @@ const Cart = ({ setShowCart }) => {
   useEffect(() => {
     // Configura el oyente de Firebase Firestore cuando el carrito se abre
     if (!currentUser) return;
+
     const cartDocRef = doc(db, "carritos", currentUser.uid);
     const unsubscribe = onSnapshot(cartDocRef, (docSnapshot) => {
       if (docSnapshot.exists()) {
@@ -45,27 +44,51 @@ const Cart = ({ setShowCart }) => {
     };
   }, []);
 
-  const cartItems = (
-    <ul className="  max-h-80 overflow-auto">
-      {products.map((product) => {
-        return (
-          <CartItem
-            key={product.id}
-            id={product.id}
-            name={product.brand}
-            price={product.precio}
-            imagen={product.imageUrl}
-            amount={product.cantidad}
-          />
-        );
-      })}
-    </ul>
-  );
+  let cartItems = null;
 
-  const totalAmount = products.reduce((curNumber, item) => {
-    return curNumber + Number(item.precio) * item.cantidad;
-  }, 0);
+  let totalAmount = null;
 
+  if (currentUser) {
+    cartItems = (
+      <ul className="  max-h-80 overflow-auto">
+        {products?.map((product) => {
+          return (
+            <CartItem
+              key={product.id}
+              id={product.id}
+              name={product.brand}
+              price={product.precio}
+              imagen={product.imageUrl}
+              amount={product.cantidad}
+            />
+          );
+        })}
+      </ul>
+    );
+    totalAmount = products?.reduce((curNumber, item) => {
+      return curNumber + Number(item.precio) * item.cantidad;
+    }, 0);
+  } else {
+    cartItems = (
+      <ul className="  max-h-80 overflow-auto">
+        {productsLocalStorage?.map((product) => {
+          return (
+            <CartItem
+              key={product.id}
+              id={product.id}
+              name={product.brand}
+              price={product.precio}
+              imagen={product.imageUrl}
+              amount={product.cantidad}
+            />
+          );
+        })}
+      </ul>
+    );
+    totalAmount = productsLocalStorage?.reduce((curNumber, item) => {
+      return curNumber + Number(item.precio) * item.cantidad;
+    }, 0);
+  }
   const orderClickHandler = () => {
     const totalproducts = [
       ...new Set(products.map((product) => product.brand)),
@@ -96,7 +119,34 @@ const Cart = ({ setShowCart }) => {
                 className=" text-white rounded-2xl shadow-none hover:scale-105"
               />
 
-              {products.length > 0 && (
+              {products?.length > 0 && (
+                <Button
+                  className=" bg-white rounded-2xl transform hover:scale-105 shadow-none border-solid border-2 border-black "
+                  text="Order"
+                  onClick={orderClickHandler}
+                />
+              )}
+            </section>
+          </article>
+        </summary>
+      ) : productsLocalStorage?.length ? (
+        <summary className="bg-gray-300  p-10 max-md:w-full max-md:h-full rounded-lg animate-slide-down">
+          {cartItems}
+          <article className="flex max-md:flex-col-reverse mt-8  font-bold gap-20 text-1.5rem my-4">
+            <div className=" flex gap-5 items-center">
+              <span className="font-bold text-2xl">Total Amount:</span>
+              <span className="font-bold text-2xl">{totalAmount}$</span>
+            </div>
+            <section className="flex max-md:flex-col-reverse  gap-4 pb-3">
+              <Button
+                onClick={() => {
+                  setShowCart(false);
+                }}
+                text={"Close"}
+                className=" text-white rounded-2xl shadow-none hover:scale-105"
+              />
+
+              {productsLocalStorage.length > 0 && (
                 <Button
                   className=" bg-white rounded-2xl transform hover:scale-105 shadow-none border-solid border-2 border-black "
                   text="Order"
