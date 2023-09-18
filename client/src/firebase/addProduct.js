d4b5f2bfe59fcaecb7c8426e3e8aa2cc18b64aec
alecanonm <al3jandrocan0n@gmail.com>
2023-09-17 21:53:50 -0500

    Se arreglo un bug en el carrit

--- client/src/firebase/addProduct.js
+++ client/src/firebase/addProduct.js
@@ -3,14 +3,45 @@ import { db } from "./credenciales";
 
 const addProduct = async (uid, producto) => {
   try {
-    const carritoDocRef = doc(db, "carritos", uid); // Referencia al documento del carrito
+    const carritoDocRef = doc(db, "carritos", uid);
     // Consulta el documento actual del carrito
     const carritoDoc = await getDoc(carritoDocRef);
     // Si el documento del carrito existe, actualiza su contenido
-    carritoDoc.exists() &&
-      (await updateDoc(carritoDocRef, {
-        productos: arrayUnion(producto), // Agrega el producto al arreglo de productos
-      }));
+
+    if (Array.isArray(producto)) {
+      console.log("Es un array");
+      const extingProducts = carritoDoc.data().productos;
+      if (extingProducts.length > 0) {
+        const combinedProducts = [...extingProducts, ...producto];
+        const conteo = [];
+
+        for (const producto of combinedProducts) {
+          const existe = conteo.find((p) => p.id === producto.id);
+
+          if (existe) {
+            existe.cantidad += producto.cantidad;
+          } else {
+            conteo.push(producto);
+          }
+        }
+        conteo &&
+          (await updateDoc(carritoDocRef, {
+            productos: conteo,
+          }));
+      } else {
+        carritoDoc.exists() &&
+          producto &&
+          (await updateDoc(carritoDocRef, {
+            productos: arrayUnion(producto),
+          }));
+      }
+    } else {
+      carritoDoc.exists() &&
+        producto &&
+        (await updateDoc(carritoDocRef, {
+          productos: arrayUnion(producto),
+        }));
+    }
   } catch (error) {
     console.error("Error al agregar producto al carrito:", error);
   }
