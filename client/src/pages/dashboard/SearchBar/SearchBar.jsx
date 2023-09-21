552e9653a9514e1980f2e7d1f4139667f82c8faf
Wilialberth Jiménez <7jimenez.w@gmail.com>
2023-09-21 01:54:03 -0300

    SearchBar_Dashboard

---
+++ client/src/pages/dashboard/SearchBar/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const SearchBar = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchInputChange = (e) => {
+    const newSearch = e.target.value;
+    setSearchQuery(newSearch);
+    onSearch(newSearch);
+  };
+
+  return (
+    <div className="mb-4">
+      <input
+        type="text"
+        placeholder="Buscar..."
+        value={searchQuery}
+        onChange={handleSearchInputChange}
+        className="w-full border border-gray-300 rounded-md p-2"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
+
--- client/src/pages/dashboard/pages/Products/Products_Admin.jsx
+++ client/src/pages/dashboard/pages/Products/Products_Admin.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import SearchBar from "../../SearchBar/SearchBar";
 
 const Products_Admin = () => {
   const [motos, setMotos] = useState([]);
   const [selectedMotos, setSelectedMotos] = useState([]);
   const [selectAll, setSelectAll] = useState(false);
+  const [filteredMotos, setFilteredMotos] = useState([]);
+
   const selectedMotoIds = selectedMotos.map((selectedMoto) => selectedMoto.id);
 
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
@@ -57,10 +60,29 @@ const Products_Admin = () => {
     }
   };
 
+  const handleSearch = (searchQuery) => {
+    const searchText = searchQuery.toLowerCase();
+    const filteredMotos = motos.filter((moto) => {
+      const motoId = moto.id ? moto.id.toString().toLowerCase() : "";
+      return (
+        motoId.includes(searchText) ||
+        (moto.motoModel && moto.motoModel.toLowerCase().includes(searchText)) ||
+        (moto.brand && moto.brand.name && moto.brand.name.toLowerCase().includes(searchText)) ||
+        (moto.tipo && moto.tipo.name && moto.tipo.name.toLowerCase().includes(searchText))
+      );
+    });
+    setFilteredMotos(filteredMotos);
+  };
+  
+  
+  
+
   console.log(screenWidth);
+
   return (
     <div className="min-h-full bg-slate-100 p-4">
       <h1>Administrador de Productos</h1>
+      {/* <SearchBar onSearch={handleSearch} /> */}
       <table className={`w-full border-2 shadow-sm duration-300`}>
         <thead
           className={`h-[40px] bg-[#C63D05] text-white ${
@@ -104,58 +126,61 @@ const Products_Admin = () => {
             screenWidth >= 1220 ? "" : "duration-300 text-[14px]"
           }`}
         >
-          {motos.map((moto) => (
-            <tr className="hover:text-blue-400 h-[75px]" key={moto?.id}>
-              <td className="text-center w-1/8">
-                <input
-                  type="checkbox"
-                  className="w-4 h-4 ml-1"
-                  onChange={() => toggleSelectMoto(moto)}
-                  checked={selectedMotos.includes(moto)}
-                />
-              </td>
-              {screenWidth >= 900 && (
-                <td className="text-center w-1/8 font-bold ml-1">{moto?.id}</td>
-              )}
-              {screenWidth >= 768 && (
+          {motos.map((moto) => {
+            console.log(moto);
+            return (
+              <tr className="hover:text-blue-400 h-[75px]" key={moto?.id}>
                 <td className="text-center w-1/8">
-                  <img
-                    src={moto?.imageUrl[0]}
-                    alt="Moto"
-                    width="100"
-                    className="mx-auto"
+                  <input
+                    type="checkbox"
+                    className="w-4 h-4 ml-1"
+                    onChange={() => toggleSelectMoto(moto)}
+                    checked={selectedMotos.includes(moto)}
                   />
                 </td>
-              )}
-              <td className="text-center w-1/8 font-bold uppercase hover:text-[#C63D05] cursor-pointer">
-                {moto?.brand?.name} - {moto?.motoModel}
-              </td>
-              {screenWidth >= 900 && (
-                <td className="text-center w-1/8 font-bold uppercase">
-                  {moto?.tipo?.name}
+                {screenWidth >= 900 && (
+                  <td className="text-center w-1/8 font-bold ml-1">{moto?.id}</td>
+                )}
+                {screenWidth >= 768 && (
+                  <td className="text-center w-1/8">
+                    <img
+                      src={moto?.imageUrl[0]}
+                      alt="Moto"
+                      width="100"
+                      className="mx-auto"
+                    />
+                  </td>
+                )}
+                <td className="text-center w-1/8 font-bold uppercase hover:text-[#C63D05] cursor-pointer">
+                  {moto?.brand?.name} - {moto?.motoModel}
                 </td>
-              )}
-              <td className="text-center w-1/8 font-bold">
-                {moto?.stock} {screenWidth >= 400 && "Unds"}
-              </td>
-              <td className="text-center w-1/8 font-bold text-blue-600 mr-1">
-                {parseFloat(moto?.precio).toLocaleString("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                  minimumFractionDigits: 2,
-                })}
-              </td>
-              {screenWidth >= 1220 && (
-                <td
-                  className={`text-center w-1/8 font-bold uppercase ${
-                    moto?.deleted ? "text-red-600" : "text-green-600"
-                  }`}
-                >
-                  {moto?.deleted ? "Inactivo" : "Activo"}
+                {screenWidth >= 900 && (
+                  <td className="text-center w-1/8 font-bold uppercase">
+                    {moto?.tipo?.name}
+                  </td>
+                )}
+                <td className="text-center w-1/8 font-bold">
+                  {moto?.stock} {screenWidth >= 400 && "Unds"}
                 </td>
-              )}
-            </tr>
-          ))}
+                <td className="text-center w-1/8 font-bold text-blue-600 mr-1">
+                  {parseFloat(moto?.precio).toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                    minimumFractionDigits: 2,
+                  })}
+                </td>
+                {screenWidth >= 1220 && (
+                  <td
+                    className={`text-center w-1/8 font-bold uppercase ${
+                      moto?.deleted ? "text-red-600" : "text-green-600"
+                    }`}
+                  >
+                    {moto?.deleted ? "Inactivo" : "Activo"}
+                  </td>
+                )}
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
