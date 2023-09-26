f9b4b76264b0dfa39f041843d7d9cf9d91a1a254
Hengers Rosario <hengersrosario.m@gmail.com>
2023-09-26 17:59:54 -0400

    fix: controller editMoto

--- api/src/controllers/editMoto.js
+++ api/src/controllers/editMoto.js
@@ -1,22 +1,44 @@
-const { Moto } = require("../db");
+const { Moto, Color } = require("../db");
 
-// Controlador para actualizar una moto por su ID
 async function editMoto(req, res) {
   try {
     const motoId = req.params.id;
-    const { precio, imageUrl } = req.body;
-
-    const moto = await Moto.findByPk(motoId);
+    const { precio, imageUrl, colorDisponible } = req.body;
+    console.log(req.body);
+    const moto = await Moto.findByPk(motoId, {
+      include: [Color], // Incluye la relación con los colores asociados
+    });
 
     if (!moto) {
-      throw res.status(404).json({ error: "Moto no encontrada" });
+      return res.status(404).json({ error: "Moto no encontrada" });
     }
 
-    // Actualizar información de la moto
-
+    // Actualizar información de la moto (precio e imageUrl)
     await moto.update({ precio, imageUrl });
 
-    res.json(moto);
+    // Función para agregar un color por nombre y obtener su ID
+    async function addColorByName(colorName) {
+      let color = await Color.findOne({ where: { name: colorName } });
+      if (!color) {
+        color = await Color.create({ name: colorName });
+      }
+      return color.id;
+    }
+
+    // Si existen colores disponibles, asócialos a la moto
+    if (colorDisponible && colorDisponible.length > 0) {
+      for (const colorName of colorDisponible) {
+        const colorId = await addColorByName(colorName);
+        await moto.addColor(colorId);
+      }
+    }
+
+    // Devuelve la moto actualizada, incluyendo los colores asociados
+    const updatedMoto = await Moto.findByPk(motoId, {
+      include: [Color],
+    });
+
+    res.json(updatedMoto);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "No se pudo actualizar la moto" });
