5960a0652095f829ee99de87f8f87018018a37bb
Hengers3012 <hengersrosario.m@gmail.com>
2023-09-27 21:54:04 -0400

    fix: Component Invoice

--- client/src/components/Invoice/Invoice.jsx
+++ client/src/components/Invoice/Invoice.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useContext } from "react";
 import { userAuth } from "../../context/Auth-context";
 
+import { parsePhoneNumberFromString } from "libphonenumber-js";
+import { format, parse } from "date-fns";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
-
-import { parsePhoneNumberFromString } from "libphonenumber-js";
 
 import Wrapper from "../../helper/Wrapper";
 
@@ -12,10 +12,20 @@ import { FaWindowClose, FaFileDownload } from "react-icons/fa";
 import logoCerradoWhite from "../../assets/Logo_Mitsumichi_White.png";
 import logoCerradoBlack from "../../assets/Logo_Mitsumichi.png";
 
+function convertirFechaLarga(fechaStr) {
+  // Parsea la cadena de fecha en un objeto de fecha
+  const fecha = parse(fechaStr, "d/M/yyyy", new Date());
+
+  // Formatea la fecha
+  const fechaLarga = format(fecha, "EEEE, dd MMMM yyyy");
+
+  return fechaLarga;
+}
+
 const Invoice = ({ facturaData, onClose }) => {
   const { user } = useContext(userAuth);
-  const [capturedImage, setCapturedImage] = useState(null); // Estado para la imagen capturada
-  console.log(facturaData);
+  const fechaStr = facturaData?.today;
+  const fechaLarga = convertirFechaLarga(fechaStr);
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   const elementosComprados = Object.values(facturaData).filter(
@@ -49,7 +59,7 @@ const Invoice = ({ facturaData, onClose }) => {
   );
   const captureComponentAsPDF = () => {
     const scaleWidth = 1; // Escala para la captura
-    const screenHeight = 2.8;
+    const screenHeight = 2;
 
     // Calcula el ancho y alto del contenedor multiplicado por la escala
     const width =
@@ -88,20 +98,20 @@ const Invoice = ({ facturaData, onClose }) => {
 
   return (
     <Wrapper>
-      <div className="relative flex flex-col min-w-[80%] min-h-[85%] max-h-[85%] items-center justify-center p-4 overflow-y-auto">
+      <div className="relative flex flex-col min-w-[80%] min-h-[90%] max-h-[90%] mt-16 items-center justify-center p-4">
         <div
-          className={`relative flex min-w-[80%] min-h-[85%] max-h-[85%] pb-16 py-4 px-10 bg-white rounded-md justify-between overflow-hidden`}
+          className={`relative flex w-[1150px] min-h-[85%] max-h-[85%] pb-8 pt-4 px-10 bg-white rounded-md justify-between overflow-hidden overflow-y-scroll scrollbar-gutter`}
         >
           <button
             onClick={onClose}
-            className="absolute top-4 right-4 text-2xl text-[#C63D05] transition duration-300 hover:text-[#252525] hover:scale-110"
+            className="absolute top-4 right-4 text-2xl text-[#C63D05] transition duration-300 hover:text-[#252525] hover:scale-110 z-10"
           >
             <FaWindowClose size={30} />
           </button>
-          <div id="invoice-container" className="relative ">
+          <div id="invoice-container" className="relative">
             <p className="absolute left-4 ">
               <span className="font-semibold">Fecha de realización: </span>
-              {facturaData?.today}
+              {fechaLarga}
             </p>
             <div className="flex flex-row mt-12 justify-between items-center w-[100%]">
               <div className="flex flex-row w-[500px] h-[250px] bg-[#C63D05] mx-5 rounded-md p-4">
@@ -118,53 +128,61 @@ const Invoice = ({ facturaData, onClose }) => {
                 </section>
               </div>
 
-              <div className="flex flex-row w-[500px] h-[250px] mx-5 rounded-md p-4 border-2 border-[#252525]">
-                <section className="flex flex-col justify-center">
-                  <ul className="text-xl">
-                    <li>
+              <div className="flex flex-row w-[500px] h-[250px] mx-5 rounded-md px-6 border-2 border-[#252525]">
+                <div className="flex flex-col justify-between py-4 text-xl ">
+                  <section>
+                    <p>
                       <span className="text-2xl font-semibold">
                         No. Factura:{" "}
                       </span>
                       {"MIT"}
                       {facturaData?.id}
-                    </li>
-                    <br />
-                    <li>
+                    </p>
+                  </section>
+
+                  <section>
+                    <p>
                       <span className="text-2xl font-semibold">Cliente: </span>
                       {user?.data?.name} {user?.data?.apellido}
-                    </li>
-                    <li>
+                    </p>
+                    <p>
                       <span className="text-2xl font-semibold">
                         Dirección:{" "}
                       </span>
                       {user?.data?.direccion}
-                    </li>
-                    <li>
+                    </p>
+                    <p>
                       <span className="text-2xl font-semibold">Tel: </span>
                       {phoneNumber.formatInternational()}
-                    </li>
-                    <li>
+                    </p>
+                    <p>
                       <span className="text-2xl font-semibold">Email: </span>
                       {user?.email}
-                    </li>
-                  </ul>
-                </section>
+                    </p>
+                  </section>
+                </div>
               </div>
             </div>
             <div className="flex flex-col mt-12 mx-5">
               <table className="w-full">
-                <thead className="bg-[#C63D05] h-10">
-                  <tr>
-                    <th className="text-white w-[10%] uppercase">
+                <thead className="bg-[#C63D05] h-fit">
+                  <tr className="py-5">
+                    <th className="text-white w-[10%] pb-2 uppercase">
                       Id Producto
                     </th>
-                    <th className="text-white w-[35%] uppercase">
+                    <th className="text-white w-[35%] pb-2 uppercase">
                       Descripción del Producto
                     </th>
-                    <th className="text-white w-[10%] uppercase">Categoria</th>
-                    <th className="text-white w-[10%] uppercase">Cantidad</th>
-                    <th className="text-white w-[10%] uppercase">Precio Und</th>
-                    <th className="text-white w-[10%] uppercase">Total</th>
+                    <th className="text-white w-[10%] pb-2 uppercase">
+                      Categoria
+                    </th>
+                    <th className="text-white w-[10%] pb-2 uppercase">
+                      Cantidad
+                    </th>
+                    <th className="text-white w-[10%] pb-2 uppercase">
+                      Precio Und
+                    </th>
+                    <th className="text-white w-[10%] pb-2 uppercase">Total</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -182,7 +200,7 @@ const Invoice = ({ facturaData, onClose }) => {
                         </td>
                         <td className="text-center w-[10%]">
                           {producto?.cantidad}{" "}
-                          {producto?.cantidad > 1 ? "Unds" : "Und"}
+                          {producto?.cantidad > 1 ? "UNDS" : "UND"}
                         </td>
                         <td className="text-center w-[10%]">
                           <div className="flex flex-row w-full justify-between px-5 ">
@@ -223,8 +241,8 @@ const Invoice = ({ facturaData, onClose }) => {
               <ul className="mt-10 font-bold text-[#252525]">
                 <li className="flex flex-row justify-between border-b border-gray-300 h-10">
                   <div className="flex flex-row w-full justify-between ">
-                    <section className="w-[100%]">
-                      <p>Sub Total:</p>
+                    <section className="w-[100%] pl-6">
+                      <p className="uppercase">Sub Total:</p>
                     </section>
                     <section className="flex flex-row w-48 justify-between pl-12 pr-5">
                       <p>$</p>
@@ -240,8 +258,8 @@ const Invoice = ({ facturaData, onClose }) => {
 
                 <li className="flex flex-row justify-between border-b border-gray-300 h-10">
                   <div className="flex flex-row w-full justify-between">
-                    <section className="w-[100%]">
-                      <p>Descuento:</p>
+                    <section className="w-[100%] pl-6">
+                      <p className="uppercase">Descuento:</p>
                     </section>
                     <section className="flex flex-row w-48 justify-between pl-12 pr-5">
                       <p>$</p>
@@ -257,10 +275,10 @@ const Invoice = ({ facturaData, onClose }) => {
 
                 <li className="flex flex-row justify-between text-3xl mt-4">
                   <div className="flex flex-row w-full justify-between">
-                    <section className="w-[100%]">
-                      <p>Total:</p>
+                    <section className="w-[100%] pl-6">
+                      <p className="uppercase">Total:</p>
                     </section>
-                    <section className="flex flex-row w-60 justify-between pl-12 pr-5">
+                    <section className="flex flex-row w-72 justify-between pl-12 pr-5">
                       <p>$</p>
                       <p>
                         {parseFloat(subtotal).toLocaleString("en-US", {
@@ -290,14 +308,15 @@ const Invoice = ({ facturaData, onClose }) => {
               </section>
             </div>
           </div>
-          <button
-            type="button"
-            onClick={captureComponentAsPDF}
-            className="absolute bottom-4 right-4 flex items-center justify-center bg-[#C63D05] p-2 rounded-lg duration-300 text-lg font-semibold text-white shadow-sm hover:shadow-sm shadow-[#202020] hover:text-gray-900 hover:bg-[#ff6600]"
-          >
-            <span className="mr-2">Descargar</span> <FaFileDownload size={20} />
-          </button>
         </div>
+
+        <button
+          type="button"
+          onClick={captureComponentAsPDF}
+          className="absolute bottom-4 -right-6 flex items-center justify-center p-2 bg-white text-[#C63D05] font-bold  rounded-lg shadow-sm duration-300 hover:shadow-sm shadow-[#202020] hover:text-gray-900 hover:bg-[#ff6600] hover:scale-105"
+        >
+          <span className="mr-2">Descargar</span> <FaFileDownload size={20} />
+        </button>
       </div>
     </Wrapper>
   );
