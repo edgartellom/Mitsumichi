828c44058215c7f858642f25257f262ba7eea5a3
Hengers Rosario <hengersrosario.m@gmail.com>
2023-09-29 15:03:22 -0400

    Componente Edit User

--- client/src/pages/dashboard/pages/Users/EditUser.jsx
+++ client/src/pages/dashboard/pages/Users/EditUser.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Wrapper from "../../../../helper/Wrapper";
+import { FaWindowClose } from "react-icons/fa";
 
 const EditUser = ({ userData, onClose }) => {
   console.log(userData);
   return (
     <Wrapper>
-      {/* Mostrar los datos del usuario */}
-      <div>Nombre de usuario: {userData?.data?.name}</div>
-      <div>Email: {userData?.email}</div>
-      {/* Resto de los datos del usuario */}
+      <div className="overflow-y-scroll scrollbar-gutter">
+        <div className="relative flex w-[450px] h-[600px] bg-white p-10 rounded-md">
+          <button
+            onClick={onClose}
+            className="absolute top-2 right-2 text-2xl text-[#C63D05] transition duration-300 hover:text-[#252525] hover:scale-110 z-10"
+          >
+            <FaWindowClose size={30} />
+          </button>
+          <div className="w-full h-full p-4">{/* Aqui trabaja Angel*/}</div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
